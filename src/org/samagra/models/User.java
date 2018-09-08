36d78c027c0c2c23b029e1132d01ad104eb85fcb
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2018-09-08 10:50:36 +0530

    Getter and settere for name

--- src/org/samagra/models/User.java
+++ src/org/samagra/models/User.java
@@ -12,6 +12,14 @@ public class User {
 		this.age = age;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
 		return "User [age=" + age + "]";
